311ed00659de2db4f67ea0b81ba730604fb0ef12
Zubair Trabzada <ztrabzada@losaltosca.gov>
2017-03-24 17:09:30 -0700

    adding js functionality

--- app.js
+++ app.js
@@ -40,3 +40,20 @@ var todoList = {
     }
   }
 };
+
+var handlers = {
+  addTodo: function() {
+    var addTodoTextInput = document.getElementById('addTodoTextInput');
+    todoList.addTodo(addTodoTextInput.value);
+    addTodoTextInput.value = '';
+    view.displayTodos();
+  },
+  changeTodo: function() {
+    var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+    var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+    todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+    changeTodoPositionInput.value = '';
+    changeTodoTextInput.value = '';
+    view.displayTodos();
+
+};
